956d2afc477c2d3007a53606bd50138d8eed051a
sangshin <zxcv1867@naver.com>
2025-03-24 18:28:02 +0900

    ADD xor, step_function, sigmoid, relu
    page 77

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pylab as plt
+
 def AND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
@@ -28,3 +31,34 @@ def OR(x1, x2):
     else:
         return 1
 
+def XOR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int64)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
