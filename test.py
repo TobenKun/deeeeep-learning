9b33a3c413900da58e25fc6ca2f3ac199e3b91d1
sangshin <zxcv1867@naver.com>
2025-03-27 17:26:40 +0900

    3 floor neural network
    page 90

--- test.py
+++ test.py
@@ -57,8 +57,8 @@ def sigmoid(x):
 def relu(x):
     return np.maximum(0, x)
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
-plt.show()
+# x = np.arange(-5.0, 5.0, 0.1)
+# y = sigmoid(x)
+# plt.plot(x, y)
+# plt.ylim(-0.1, 1.1)
+# plt.show()
