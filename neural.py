FIX softmax func overflow
page 94

neural.py
@@ -9,9 +9,13 @@ def sigmoid(x):
 def identity_function(x):
     return x
 
-# 일반적으로 회귀에는 항등함수를, 분류에는 소프트맥스 함수를 사용한다
-# page 90
+def softmax(a):
+    c = np.max(a) # 오버플로 방지
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
 
+    return y
 
 def init_network():
     network = {}
@@ -34,6 +38,7 @@ def forward(network, x): # 신호가 순방향으로 전달되는 순전파
     z2 = sigmoid(a2)
     a3 = np.dot(z2, W3) + b3
     y = identity_function(a3)
+    #y = softmax(a3)
 
     return y
 
@@ -42,3 +47,4 @@ network = init_network()
 x = np.array([1.0, 0.5])
 y = forward(network, x)
 print(y)
+